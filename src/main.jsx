b4489c6908143095542a8f96571dd6a0d5f371d9
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-06-15 01:16:10 +0600

    Service section add

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import Header from './section/Header/Header'
 import Hero from './section/Hero/Hero'
 import Campain from './section/Campain/Campain'
+import Service from './section/Description/Service'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Hero/>
     <Campain/>
+    <Service/>
   </StrictMode>,
 )
