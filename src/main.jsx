efcc46f7f8e749a7149b744b56aa2f5cecb201c2
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-06-15 00:44:40 +0600

    Header and Hero Section Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './section/Header/Header'
+import Hero from './section/Hero/Hero'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Hero/>
   </StrictMode>,
 )
