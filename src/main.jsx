e9b7302a7f0a6850811ae4e0c2239507a5a2c534
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-06-15 00:49:45 +0600

    Campain sectionn added

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './section/Header/Header'
 import Hero from './section/Hero/Hero'
+import Campain from './section/Campain/Campain'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Hero/>
+    <Campain/>
   </StrictMode>,
 )
